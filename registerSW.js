d22608a0a124a840ba0cdf4a250aeb746fb00fd5
vllxrl <124093000+vllxrl@users.noreply.github.com>
2025-01-16 21:05:22 +0300

    Add files via upload

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Meropriyatiya-profkoma-studentov/sw.js', { scope: '/Meropriyatiya-profkoma-studentov/' })})}
